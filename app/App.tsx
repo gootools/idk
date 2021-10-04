92db459237cf4dc35dafabe0ea82450e794014ab
John Rees <john@bitsushi.com>
2021-10-04 10:58:07 +0100

    chrome manifest

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useLayoutEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  useLayoutEffect(() => {
+    const body = document?.querySelector("body");
+    if (body) {
+      body.style.width = "320px";
+      body.style.height = "568px";
+      body.style.backgroundColor = "black";
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -14,8 +23,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    // backgroundColor: "#fff",
+    // width: 500,
+    // height: 500,
   },
 });
