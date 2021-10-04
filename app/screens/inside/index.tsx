0a4e740217f974ff11f51e469a71988e0d2e5b96
John Rees <john@bitsushi.com>
2021-10-04 16:20:50 +0100

    tab icons

--- app/screens/inside/index.tsx
+++ app/screens/inside/index.tsx
@@ -1,3 +1,4 @@
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
@@ -13,10 +14,25 @@ export default function Inside() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={{
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused }) => {
+            const props = { size: 28, color: focused ? "green" : "black" };
+            switch (route.name) {
+              case "Feed":
+                return <Ionicons name="ios-notifications" {...props} />;
+              case "Wallet":
+                return <MaterialCommunityIcons name="cash-usd" {...props} />;
+              case "Settings":
+                return <Ionicons name="ios-settings" {...props} />;
+              default:
+                return (
+                  <MaterialCommunityIcons name="lightning-bolt" {...props} />
+                );
+            }
+          },
           headerShown: false,
           tabBarShowLabel: false,
-        }}
+        })}
       >
         <Tab.Screen name="Feed" component={Feed} />
         <Tab.Screen name="Wallet" component={Wallet} />
