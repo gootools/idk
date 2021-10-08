39b6c1d1c45817c0dbc1eac6a0b00aa886807ac0
John Rees <john@bitsushi.com>
2021-10-08 18:30:04 +0100

    isomorphic-webcrypto vs react-native-webview-crypto

--- app/App.native.tsx
+++ app/App.native.tsx
@@ -1,15 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import React, { Suspense } from "react";
+import React from "react";
 import "react-native-url-polyfill/auto";
-import Polyfill from "react-native-webview-crypto";
 import SharedApp from "./SharedApp";
 
 const NativeApp: React.FC = () => (
-  <Suspense fallback={null}>
-    <Polyfill />
+  <>
     <StatusBar style="light" />
     <SharedApp />
-  </Suspense>
+  </>
 );
 
 export default NativeApp;
--- app/metro.config.js
+++ app/metro.config.js
@@ -3,6 +3,7 @@ module.exports = {
     sourceExts: ["jsx", "js", "ts", "tsx", "cjs"],
     extraNodeModules: {
       buffer: require.resolve("buffer").Buffer,
+      crypto: require.resolve("isomorphic-webcrypto"),
       stream: require.resolve("readable-stream"),
     },
   },
